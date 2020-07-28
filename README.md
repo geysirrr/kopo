a4cdf0cfbd0efb2a8363ccfec88c630b97be4a75
geysirrr <geysirrr@gmail.com>
2020-07-28 21:34:16 +0900

    added mocha testing, setDefaultJosa, error handler for not korean letter

--- README.md
+++ README.md
@@ -1,8 +1,36 @@
 # kopo.js
 
-testing...
+현재 텍스트와 조사를 포함한 결과값 반환
 
 ```javascript
-const kopo = new Kopo("김연아");
-console.log(`${kopo.get("은/는")} 아름답다.`) // 김연아는 아름답다.
+const kopo = new Kopo('김연아');
+console.log(`${kopo.get('은/는')} 아름답다.`); // 김연아는 아름답다.
+```
+
+조사만 반환
+
+```javascript
+const kopo = new Kopo('김연아');
+console.log(`${kopo.getOnlyPostposition('은/는')}.`); // 는
+```
+
+마지막 글자가 받침이 있는지 여부 확인
+
+```javascript
+const kopo = new Kopo('김연아');
+kopo.hasBatchim(); // false
+```
+
+텍스트가 한글이 아닐 경우 기본 조사 세팅
+
+```javascript
+Kopo.setDefaultJosaType(Kopo.josaType.BATCHIM);
+
+console.log(`${new Kopo('Lionel Messi').get('은/는')} Goat 이다.`);
+// Lionel Messi은 Goat 이다.
+
+Kopo.setDefaultJosaType(Kopo.josaType.NON_BATCHIM);
+
+console.log(`${new Kopo('Lionel Messi').get('은/는')} Goat 이다.`);
+// 'Lionel Messi는 Goat 이다.'
 ```
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //  (c) 2020 geysirrr
-// TODO: to add more postposition and support array
 (function (root, Kopo) {
   if (typeof define === 'function' && define.amd) {
     define([], function () {
@@ -17,13 +16,21 @@
       this.text = text;
     }
 
-    var getLastCharCode = function (text) {
-      return text.charCodeAt(text.length - 1);
+    Kopo.josaType = {
+      BATCHIM: 'batchim',
+      NON_BATCHIM: 'nonBatchim',
     };
 
-    var postpositionsForBatchim = ['을', '은', '이', '과'];
+    Kopo.setDefaultJosaType = function (josaType) {
+      var josaTypes = getValuesFromObject(Kopo.josaType);
 
-    var postpositionsForNonBatchim = ['를', '는', '가', '와'];
+      if (josaTypes.indexOf(josaType) === -1)
+        throw new Error(
+          'Invalid Josa Type. It should be "batchim" or "nontBatchim"'
+        );
+
+      Kopo.defaultJosaType = josaType;
+    };
 
     Kopo.prototype.hasBatchim = function () {
       return (getLastCharCode(this.text) - 0xac00) % 28 > 0;
@@ -36,6 +43,9 @@
         throw new Error('Arguments must be separated by /. example: "을/를"');
       }
 
+      var firstPostposition = postpositions[0];
+      var secondPostposition = postpositions[1];
+
       var getPostposition = function (postpositions) {
         return postpositions.indexOf(firstPostposition) !== -1
           ? firstPostposition
@@ -46,8 +56,18 @@
             })();
       };
 
-      var firstPostposition = postpositions[0];
-      var secondPostposition = postpositions[1];
+      if (this.isNotKorean()) {
+        if (!Kopo.defaultJosaType)
+          throw new Error('Text is not ended with Korean Letter');
+
+        if (Kopo.defaultJosaType === Kopo.josaType.BATCHIM)
+          return getPostposition(postpositionsForBatchim);
+
+        if (Kopo.defaultJosaType === Kopo.josaType.NON_BATCHIM)
+          return getPostposition(postpositionsForNonBatchim);
+
+        throw new Error('Invalid defaultJodaType');
+      }
 
       return getPostposition(
         this.hasBatchim() ? postpositionsForBatchim : postpositionsForNonBatchim
@@ -57,6 +77,27 @@
     Kopo.prototype.get = function (postposition) {
       return this.text + this.getOnlyPostposition(postposition);
     };
+
+    Kopo.prototype.isNotKorean = function () {
+      return (
+        getLastCharCode(this.text) < 0xac00 ||
+        getLastCharCode(this.text) > 0xd7a3
+      );
+    };
+
+    var getLastCharCode = function (text) {
+      return text.charCodeAt(text.length - 1);
+    };
+
+    var getValuesFromObject = function (obj) {
+      return Object.keys(obj).map(function (key) {
+        return obj[key];
+      });
+    };
+
+    var postpositionsForBatchim = ['을', '은', '이', '과'];
+
+    var postpositionsForNonBatchim = ['를', '는', '가', '와'];
 
     return Kopo;
   })()
